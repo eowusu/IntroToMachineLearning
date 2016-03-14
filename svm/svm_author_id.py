d57712bee6cfc7d530632d485c12271e8d3275ac
ethan_ffs <ethan@fingerfoodstudios.com>
2016-03-13 22:44:36 -0700

    Complete Lecture 1: Gaussian/Naive Bayes Complete Lecture 2: Support Vector Machines

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,13 +12,30 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+Cval = 10000
+clf = SVC(kernel = "rbf", C = Cval)
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+t0 = time()
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
 
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0,3), "s"
+print "Chris:",pred.tolist().count(1)
+score = accuracy_score(pred, labels_test)
+print "With C = ", Cval, " Accuracy: ", score
+print "answer at 10:", pred[10]
+print "answer at 26:", pred[26]
+print "answer at 50", pred[50]
 
 
 
